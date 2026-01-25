aaace592879905e1ac1f538741c73dea1a56eba9
gitdar555 <daryas1987@gmail.com>
2026-01-25 21:49:34 +0100

    homework string

--- src/main/java/org/prog/session4/HomeWorkString.java
+++ src/main/java/org/prog/session4/HomeWorkString.java
@@ -1,31 +1,53 @@
 package org.prog.session4;
 
-//TODO: print only correct emails
+// TODO: print only correct emails
 // Correct email rules:
 // - at least 3 symbols before @
-// - only @ symbol
+// - only one @ symbol
 // - full domain name (must have at least one ".")
-
 
 public class HomeWorkString {
 
     public static void main(String[] args) {
+
         String[] emails = new String[]{
-                "abcdefg@gmail.com", //+
-                "a@gmail.com", //-
-                "josh@@gmail.com", //-
-                "janegmail.com", // -
-                "pete@gmail.com", //+
-                "zoe@gmailcom", //-
-                "steve@outlook.com", //+
-                "abcd@a.a", //+
-                "abcd.a@fakemail" //-
+                "abcdefg@gmail.com", // +
+                "a@gmail.com",       // -
+                "josh@@gmail.com",   // -
+                "janegmail.com",     // -
+                "pete@gmail.com",    // +
+                "zoe@gmailcom",      // -
+                "steve@outlook.com", // +
+                "abcd@a.a",          // +
+                "abcd.a@fakemail"    // -
         };
 
         for (int i = 0; i < emails.length; i++) {
-            if (emails[i].equals("abcdefg@gmail.com")) {
-                System.out.println(emails[i]);
+
+            String email = emails[i];
+
+            int firstAt = email.indexOf('@');
+            int lastAt = email.lastIndexOf('@');
+
+            // rule 2: only one '@'
+            if (firstAt == -1 || firstAt != lastAt) {
+                continue;
             }
+
+            // rule 1: at least 3 symbols before '@'
+            if (firstAt < 3) {
+                continue;
+            }
+
+            // rule 3: domain must contain '.'
+            String domain = email.substring(firstAt + 1);
+            if (!domain.contains(".")) {
+                continue;
+            }
+
+            System.out.println(email);
         }
     }
 }
+
+
